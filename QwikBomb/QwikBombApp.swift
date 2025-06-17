e62b359899d93ac975f80025b4734cdc8ef3fa3f
SunnyFlops <arav.prasad@outlook.com>
2025-06-17 16:56:04 +1200

    Initial Commit

---
+++ QwikBomb/QwikBombApp.swift
@@ -0,0 +1,17 @@
+//
+//  QwikBombApp.swift
+//  QwikBomb
+//
+//  Created by SunnyFlops on 17/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct QwikBombApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
